ac9ca5ded6db677c3a2a214284ee4e2947892840
Tyronne282 <bradburntyronne@gmail.com>
2021-04-29 10:39:34 +0100

    Added a Player controller and updated use case diagram

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class PlayerController
+{
+	/// <summary>
+    /// 
+    /// </summary>
+	public AnimatedPlayer Player { get; set; }
+
+	public Vector2 StartPosition { get; set; }
+
+	public AnimatedPlayer CreatePlayer(GraphicsDevice graphics, Texture2D playerSheet)
+	{
+
+	}
+}
